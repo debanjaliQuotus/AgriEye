d32a41841ef627fae7143b4fd4250f48a4281b63
debanjaliQuotus <debanjali.lenka@quotus.co.in>
2025-07-16 16:19:26 +0530

    server.ts modified

--- server/src/server.ts
+++ server/src/server.ts
@@ -133,10 +133,10 @@ app.use('/api/motion', motionEventRoutes);
 app.use('/api/notification', smsRoutes);
 
 app.use(express.static(path.join(__dirname, '../../client/dist')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
+
 
 
 
