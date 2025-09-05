8a06c5f50fea98448787af57873f318f56ca48ab
debanjaliQuotus <debanjali.lenka@quotus.co.in>
2025-09-05 10:46:49 +0530

    random change

--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
           }}
         />
 
-        {/* Floating orbs */}
+       
         <div className="absolute top-1/4 left-1/6 w-96 h-96 bg-white/5 rounded-full blur-3xl animate-float"></div>
         <div className="absolute bottom-1/3 right-1/6 w-80 h-80 bg-blue-400/10 rounded-full blur-2xl animate-float-delayed"></div>
 
